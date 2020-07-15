56f16d71e3c355de6d8da662d4c06f77c3922d81
Christopher Graham <chris@cdgraham.com>
2020-07-15 13:21:10 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `GrowingUpFinancially.com`,
+    description: `You don't have to wait until your 65+ to retire. Find out how you can start now and do it in less time.`,
+    author: `@cdgraham`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
